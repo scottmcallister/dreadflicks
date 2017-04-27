a865ab9efbf7fb5a951760535be6eaa2d9828ba3
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-04-27 00:26:59 -0400

    add python script to import movies into postgres

---
+++ data/import.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+dbname = "dreadflicks"
+user = "postgres"
+password = ""
+host = "127.0.0.1"
+port = 5432
+SQL_STATEMENT = """
+    COPY movie(id, title, director, jhi_year, country,
+    critic_score, user_score, poster, rt_url, imdb_rating, imdb_keywords)
+    FROM STDIN WITH
+        CSV
+        HEADER
+        DELIMITER AS ','
+    """
+con = psycopg2.connect(database=dbname, user=user, password=password,
+                       host=host, port=port)
+cur = con.cursor()
+f = open('./movies.csv')
+cur.copy_expert(sql=SQL_STATEMENT, file=f)
+con.commit()
+con.close()
