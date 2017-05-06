8c4c327ae104c08ebe996101ce6e79c36fea54b4
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-05-05 22:08:50 -0400

    load first page of movies on search page

--- src/main/webapp/app/search/search.state.js
+++ src/main/webapp/app/search/search.state.js
@@ -10,9 +10,10 @@
     function stateConfig($stateProvider) {
         $stateProvider.state('search', {
             parent: 'app',
-            url: '/search',
+            url: '/search?page&sort&search',
             data: {
-                authorities: []
+                authorities: [],
+                pageTitle: 'Movie Search'
             },
             views: {
                 'content@': {
@@ -20,6 +21,28 @@
                     controller: 'SearchController',
                     controllerAs: 'vm'
                 }
+            },
+            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                },
+                search: null
+            },
+            resolve: {
+                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort),
+                        search: $stateParams.search
+                    };
+                }]
             }
         });
     }
