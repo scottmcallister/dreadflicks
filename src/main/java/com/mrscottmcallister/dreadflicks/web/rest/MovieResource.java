4a28cbfa9f29df32e966ceb5c88c01a54af19fcc
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-05-20 22:46:15 -0400

    make movies read only

--- src/main/java/com/mrscottmcallister/dreadflicks/web/rest/MovieResource.java
+++ src/main/java/com/mrscottmcallister/dreadflicks/web/rest/MovieResource.java
@@ -48,50 +48,6 @@ public class MovieResource {
     }
 
     /**
-     * POST  /movies : Create a new movie.
-     *
-     * @param movie the movie to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new movie, or with status 400 (Bad Request) if the movie has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PostMapping("/movies")
-    @Timed
-    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) throws URISyntaxException {
-        log.debug("REST request to save Movie : {}", movie);
-        if (movie.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "idexists", "A new movie cannot already have an ID")).body(null);
-        }
-        Movie result = movieRepository.save(movie);
-        movieSearchRepository.save(result);
-        return ResponseEntity.created(new URI("/api/movies/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
-
-    /**
-     * PUT  /movies : Updates an existing movie.
-     *
-     * @param movie the movie to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated movie,
-     * or with status 400 (Bad Request) if the movie is not valid,
-     * or with status 500 (Internal Server Error) if the movie couldnt be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PutMapping("/movies")
-    @Timed
-    public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie) throws URISyntaxException {
-        log.debug("REST request to update Movie : {}", movie);
-        if (movie.getId() == null) {
-            return createMovie(movie);
-        }
-        Movie result = movieRepository.save(movie);
-        movieSearchRepository.save(result);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, movie.getId().toString()))
-            .body(result);
-    }
-
-    /**
      * GET  /movies : get all the movies.
      *
      * @param pageable the pagination information
@@ -121,21 +77,6 @@ public class MovieResource {
     }
 
     /**
-     * DELETE  /movies/:id : delete the "id" movie.
-     *
-     * @param id the id of the movie to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
-    @DeleteMapping("/movies/{id}")
-    @Timed
-    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
-        log.debug("REST request to delete Movie : {}", id);
-        movieRepository.delete(id);
-        movieSearchRepository.delete(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
-    }
-
-    /**
      * SEARCH  /_search/movies?query=:query : search for the movie corresponding
      * to the query.
      *
@@ -145,12 +86,21 @@ public class MovieResource {
      */
     @GetMapping("/_search/movies")
     @Timed
-    public ResponseEntity<List<Movie>> searchMovies(@RequestParam String query, @RequestParam Integer criticMax, @ApiParam Pageable pageable) {
+    public ResponseEntity<List<Movie>> searchMovies(@RequestParam String query,
+                                                    @RequestParam Integer criticMax,
+                                                    @RequestParam Integer criticMin,
+                                                    @RequestParam Integer userMax,
+                                                    @RequestParam Integer userMin,
+                                                    @RequestParam Integer yearMax,
+                                                    @RequestParam Integer yearMin,
+                                                    @ApiParam Pageable pageable) {
         log.debug("REST request to search for a page of Movies for query {}", query);
         Page<Movie> page = movieSearchRepository.search(
             boolQuery()
                 .must(queryStringQuery(query))
-                .must(rangeQuery("criticScore").lte(criticMax)), pageable);
+                .must(rangeQuery("criticScore").lte(criticMax).gte(criticMin))
+                .must(rangeQuery("userScore").lte(userMax).gte(userMin))
+                .must(rangeQuery("year").lte(userMax).gte(userMin)), pageable);
         HttpHeaders headers = PaginationUtil.generateSearchPaginationHttpHeaders(query, page, "/api/_search/movies");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
