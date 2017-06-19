93432cff584485b66ec57e0124b06c5b9f076d63
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-06-19 14:53:09 -0400

    connect to Elasticsearch using Jest

--- src/main/java/com/mrscottmcallister/dreadflicks/config/ElasticsearchConfiguration.java
+++ src/main/java/com/mrscottmcallister/dreadflicks/config/ElasticsearchConfiguration.java
@@ -2,10 +2,11 @@ package com.mrscottmcallister.dreadflicks.config;
 
 import java.io.IOException;
 
-import org.elasticsearch.client.Client;
+import com.github.vanroy.springdata.jest.JestElasticsearchTemplate;
+import com.github.vanroy.springdata.jest.mapper.DefaultJestResultsMapper;
+import io.searchbox.client.JestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
@@ -16,8 +17,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ElasticsearchConfiguration {
 
     @Bean
-    public ElasticsearchTemplate elasticsearchTemplate(Client client, Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
-        return new ElasticsearchTemplate(client, new CustomEntityMapper(jackson2ObjectMapperBuilder.createXmlMapper(false).build()));
+    public JestElasticsearchTemplate elasticsearchTemplate(JestClient client, Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
+        return new JestElasticsearchTemplate(client, new DefaultJestResultsMapper(
+            new CustomEntityMapper(jackson2ObjectMapperBuilder.createXmlMapper(false).build())));
     }
 
     public class CustomEntityMapper implements EntityMapper {
